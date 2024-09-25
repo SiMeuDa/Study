a28ad439337f2f2df6ddc2dabf39e6b654d26873
SiMeuDa <yeskwon05@dgu.ac.kr>
2024-09-25 14:13:32 +0900

    Update 0925.c

--- 0925.c
+++ 0925.c
@@ -1,20 +1,62 @@
 #include <stdio.h>
 
 #include <limits.h>
-
 LeeO1(void)
 {
-  printf("char min : %d, Max : %d", CHAR_MIN, CHAR_MAX);
-  printf("short min : %d, Max : %d", SHRT_MIN, SHRT_MAX);
-  printf("int min : %d, Max : %d", INT_MIN, INT_MAX);
-  printf("long mon : %d, Max : %d", LONG_MIN, LONG_MAX);
+    printf("char min : %d, Max : %d", CHAR_MIN, CHAR_MAX);
+    printf("short min : %d, Max : %d", SHRT_MIN, SHRT_MAX);
+    printf("int min : %d, Max : %d", INT_MIN, INT_MAX);
+    printf("long mon : %d, Max : %d", LONG_MIN, LONG_MAX);
 
-  return 0;
+    return 0;
+}
+
+LeeO2(void)
+{
+    signed char num1=130;
+    unsigned char num2=130;
+
+    printf("%d\t%d\n", num1, num2);
+
+    return 0;
+//-126  130
+}
+
+LeeO3(void)
+{
+    char num1 = -129;
+    char num2 = 128;
+
+    printf("#UnderFlow : %d\t OverFlow : %d\n", num1, num2);
+
+    char num1 = -130;
+    char num2 = 129;
+
+    printf("#UnderFlow : %d\t OverFlow : %d\n", num1, num2);
+
+    return 0;
 }
 
 int main(void)
 {
-  LeeO1();
+    int a;
 
-  return 0;
+    printf("Which function call?: ");
+    scanf("%d", &a);
+
+    switch(a)
+        {
+        case 1:
+            LeeO1();
+            break;
+        case 2:
+            LeeO2();
+            break;
+        case 3:
+            LeeO3();
+            break;
+        default:
+            break;
+        }
+    return 0;
 }
