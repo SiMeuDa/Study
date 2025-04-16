1908f2784e1d8f49b930dc57e18e00e589ace0f9
SiMeuDa <yeskwon05@dgu.ac.kr>
2025-04-16 11:02:22 +0900

    Create linked_list.c

---
+++ Data Structure/Mid Exam/List/Linked List/Simple/linked_list.c
@@ -0,0 +1,28 @@
+#include "linked_list.h"
+
+void print_list(LinkedList* head){
+    for(LinkedList* p = head; p != NULL; p = p->link)
+        printf("%d->", p->data);
+    printf("NULL\n");
+}
+
+int main(void)
+{
+    LinkedList* head = NULL;
+
+    init_linkedlist(head);
+
+    for(int i = 0; i < 5; i++){
+        head = insert_first(head, i);
+        print_list(head);
+    }
+
+    printf("\n%d\n\n", get_length(head));
+
+    for(int i = 0; i < 5; i++){
+        head = delete_first(head);
+        print_list(head);
+    }
+
+    return 0;
+}
