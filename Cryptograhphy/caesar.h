18bb3e9131197d13f5e2e01b1e32d81b4d0a0c41
SiMeuDa <yeskwon05@dgu.ac.kr>
2026-01-22 07:26:43 +0000

    change caesar logic, create deryption logic

--- Cryptograhphy/caesar.h
+++ Cryptograhphy/caesar.h
@@ -1,47 +1,53 @@
-#pragma once
-#include <cstring>
+#include <cctype>
+#include <string>
 
-class caesar
-{
-private:
-	int key;
-
-	char* text;
-
-	bool text_copy(char* text)
+class caesar{
+protected:
+	inline char circular(char msg, int key)
 	{
-		int t_len = strlen(text);
+		int shift = key % 26;
+		if(shift < 0)
+			shift += 26;
+		if(isdigit(msg))
+			return (msg - '0' + shift) % 26 + '0';
+		else if(isupper(msg))
+			return (msg - 'A' + shift) % 26 + 'A';
+		else if(islower(msg))
+			return (msg - 'a' + shift) % 26 + 'a';
 
-		this->text = new char[t_len + 1];
-		if(this->text == nullptr)
-			return true;
-
-		for(int i = 0; i < t_len; i++)
-			this->text[i] = text[i];
-		this->text[t_len] = '\0';
-		
-		return false;
+		return msg;
 	}
+
 public:
-	caesar(int k = 0){
-		if(k > 26)
-			k = k % 26; //key = key mod 26
-		key = k;
+	caesar() {}
+	~caesar() {}
+
+	std::string crypto(const char* msg, volatile int& key, bool isErase = true)
+	{
+		std::string text = msg;
+		int len = text.length();
+
+		for(int i = 0; i < len; i++)
+			text[i] = circular(text[i], key);
+		
+		if(isErase)
+			key = 0;
+
+		return text;
 	}
 
-	~caesar(){key = 0;}
-
-	char* crypto(char* str)
+	std::string decrypto(const char* msg, volatile int& key, bool isErase = true)
 	{
+		std::string text = msg;
+		int len = text.length();
+		volatile int de_key = 26 - key;
 
-		text_copy(str);
-		for(int i = 0; text[i] != '\0'; i++)
-		{
-			text[i] += key;
+		for(int i = 0; i < len; i++)
+			text[i] = circular(text[i], de_key);
 
-			if(text[i] > 'Z')
-				text[i] -= 26;
-		}
+		de_key = 0;
+		if(isErase)
+			key = 0;
 
 		return text;
 	}
--- Cryptograhphy/test.cpp
+++ Cryptograhphy/test.cpp
@@ -1,55 +1,20 @@
 #include <iostream>
 #include "caesar.h"
-#include "vigenere.h"
 
-int main(int argc, char** argv)
+int main(void)
 {
-	if(argc == 1)
-		return -1;
-	int caesar_key = 0;
-	std::cout << "카이사르 키: ";
-	std::cin >> caesar_key;
+	caesar c;
+	std::string msg;
+	int key;
 
-	caesar c(caesar_key);
-	std::cout << "========================== 카이사르 암호문 ============== 키: " << caesar_key << " =====" << std::endl << std::endl;
-	
-	std::cout << "원문: \t\t";
-	
-	for(int i = 1; i < argc; i++)
-		std::cout << argv[i] << " ";
-	
-	std::cout << std::endl;
+	std::cout << "평문: ";
+	std::cin >> msg;
 
-	std::cout << "암호문: \t";
-        for(int i = 1; i < argc; i++)
-		std::cout << c.crypto(argv[i]) << " ";
-       	
-	std::cout << std::endl << std::endl;
+	std::cout << "key: ";
+	std::cin >> key;
 
-	std::cout << "=======================================================================" << std::endl << std::endl << std::endl;
+	std::cout << "암호문: " << c.crypto(msg.c_str(), key) << std::endl;
+	std::cout << "복호문: " << c.decrypto(msg.c_str(), key) << std::endl;
 
-
-	char vigenere_key[100];
-	std::cout << "비즈네르 키: ";
-	std::cin >> vigenere_key;
-
-	vigenere v;
-	
-
-	std::cout << "========================== 비즈네르 암호문 ============== 키: " << vigenere_key << " =====" << std::endl << std::endl;
-
-	std::cout << "원문: \t\t";
-	
-	for(int i = 1; i < argc; i++)
-		std::cout << argv[i] << " ";
-	std::cout << std::endl;
-
-	std::cout << "암호문: \t";
-	for(int i = 1; i < argc; i++)
-		std::cout << v.cihper(argv[i], vigenere_key) << " ";
-	std::cout << std::endl << std::endl;
-
-	std::cout << "=======================================================================" << std::endl << std::endl << std::endl;
-	
 	return 0;
 }
