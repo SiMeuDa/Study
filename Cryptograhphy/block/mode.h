66378dab5cc2db94b045dd7ef8cc966fc860fe96
SiMeuDa <yeskwon05@dgu.ac.kr>
2026-01-23 09:58:28 +0000

    fix

--- Cryptograhphy/block/mode.h
+++ Cryptograhphy/block/mode.h
@@ -5,7 +5,8 @@ using namespace std;
 
 class mode{
 private:
-	char* text[4];
+	char** text;
+	int b_count;
 	int len;
 
 	inline void common(string msg)
@@ -13,11 +14,14 @@ private:
 		while(msg.length() % 4)
 			msg.append("*");
 		len = msg.length();
+		b_count = len / 4;
+		text = new char*[b_count];
 		
-		for(int i = 0; i < 4; i++){
-			text[i] = new char[len / 4 + 1];
-			for(int j = 0; j < len / 4; j++)
-				(text[i])[j] = msg[j + ((i * len) / 4)];
+		for(int i = 0; i < b_count; i++){
+			text[i] = new char[5];
+			for(int j = 0; j < b_count; j++)
+				(text[i])[j] = msg[j + ((i * b_count)];
+			(text[i])[4] = '\0';
 		}
 	}
 public:
@@ -26,22 +30,23 @@ public:
 		common(msg);
 		string result = "";
 
-		for(int i = 0; i < 4; i++){
+		for(int i = 0; i < b_count; i++){
 			result.append(/*c.crypto(text[i], ikey, false) need Encihper Logic*/);
 			delete[] text[i];
 		}
+		delete[] text;
 		len = 0;
 
 		return result;
 	}
 
-	string CBC(const char* msg, const char* key, const char* VI, bool isEncrypt = true)
+	string enCBC(const char* msg, const char* key, const char* VI)
 	{
 		common(msg);
 		string result = "";
 		string temp;
 
-		for(int i = 0; i < 4; i++)
+		for(int i = 0; i < b_count; i++)
 		{
 			if(i == 0)
 				//save VI for first block
@@ -49,33 +54,51 @@ public:
 			else
 				//save before encryted block
 				temp = text[i];
-			if(isEncrypt)
+			
+
+			for(int j = 0; j < 4; j++)
 			{
-			// Encihper
-
-				for(int j = 0; j < len / 4; j++)
-				{
-					(text[i])[j] = (text[i])[j] ^ temp[j % temp.length()];
-					//Encihper Logic
-				}
+				(text[i])[j] = (text[i])[j] ^ temp[j % temp.length()];
+				//Encihper Logic
 			}
-			else
-			{
-			//Decihper
-				for(int j = 0; j < len / 4; j++)
-				{
-					//Decihper Logic
-
-
-					(text[i])[j] = (text[i])[j] ^ temp[j % length()];
-				}
-			}
-
+			
 			result.append(text[i]);
 		}
 		//free C-style string made for encihper lpgice
-		for(int i = 0; i < 4; i++)
+		for(int i = 0; i < b_count; i++)
 			delete[] text[i];
+		delete[] text;
+
+		return result;
+	}
+
+	string deCBC(const char* msg, const char* key, const char* VI)
+	{
+		common(msg);
+		string result = "";
+		string temp, before;
+
+		for(int i = 0; i < b_count; i++)
+		{
+			if(i == 0)
+				//save VI for first block
+				temp = VI;
+			else
+				//save before decryted block
+				temp = before;
+
+			//decihper logic
+
+			before = text[i];
+			
+			for(int j = 0; j < 4; j++)
+				(text[i]) = temp[j] ^ (text[i])[j];
+		}
+		//free C-style string made for decihper logic
+		for(int i = 0; i < b_count; i++)
+			delete[] text[i];
+		delete[] text;
+		before = "";
 	
 		return result;
 	}
