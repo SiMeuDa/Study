59a4bd4f24e721914342de09e3b2b1bac92b0a00
SiMeuDa <yeskwon05@dgu.ac.kr>
2026-03-28 03:07:12 +0000

    find Segmentation fault error

--- 26/C2603/cipher/main.cpp
+++ 26/C2603/cipher/main.cpp
@@ -11,117 +11,70 @@
 #include <cctype>
 using namespace std;
 
-inline bool isValidPath(const std::string& path);
+//Checking Function
+inline bool isValidPath(const std::string&);
 inline bool isStrDigit(const char*, int);
+//Mutual Function
+inline void chckArgu(int, char**);
+inline void takeKey(int, char**, uint64_t&, uint64_t&);
+//Divided to En/De Function
+inline void readValue(const char*, std::string&);
+inline void readValue(const char*, std::vector<uint64_t>&);
+inline void writeValue(const char*, const std::vector<uint64_t>);
+inline void writeValue(const char*, const std::string);
 
 int main(int argc, char* argv[])
 {
-	//take error messge
-	string errMsg;
-	cipher c;
 	try{
-		//isEncryption: 1, filePath: 1, key: 1
-		if(((argc == 7) || (argc == 8)) && (argv[3] == "-f"sv) && (argv[5] == "-k"sv) && (argv[1] == "-en"sv))
-		{//Standard DES section
-		//foam: ./[file_name] -en [1/0] -f [file_path] -k [key]
-			if(!isValidPath(argv[4]))
-				throw std::invalid_argument("[Invalid Input]: Wrong File Path");
+		//check Argument
+		//If Invalid Argument, throw exception
+		chckArgu(argc, argv);
 
-			fstream file;
-			uint64_t key, key2;
-			char buffer[128];
-			string str;
-
-
-			//UINT64_MAX = 18,446,744,073,709,551,615 = 0xFFFFFFFFFFFFFFFF<F12
-			try{
-				if((argc == 9) && (argv[6] != argv[7]))
-					key2 = stoull(argv[7]);
-				else
-					key2 = 0;
-
-				key = stoull(argv[6]);
-			}catch(const out_of_range& e)
-			{
-				throw std::overflow_error("[Runtime Error]: Key Value is too Big");
-			}
-			catch(const invalid_argument& e)
-			{
-				throw invalid_argument("[Invalid Input]: Key Value must be ULL Type");
-			}
-
-			if(argv[2] == "1"sv)
-			{//Encryption
-				//open file for binary -> reduce program running time
-				file.open(argv[4], ios::in | ios::binary);
+		cipher c;
+		uint64_t key, key2;
+		string str;
+	
+		//take key
+		//If OverFlow or Invalid Value, throw exception
+		takeKey(argc, argv, key, key2);
+		
+		//check cryption argument
+		if(argv[2] == "1"sv)
+		{//Encryption
 			
-				if(file.fail())
-					throw std::invalid_argument("[Invalid Input]: Failed to Open File");
+			//read value from file
+			readValue(argv[4], str);
 
-				//take file msg
-				while(file.read(buffer, sizeof(buffer)))
-					str.append(buffer, file.gcount());	//take real read count, append that
+			//encrypt logic
+			std::vector<uint64_t> vecResult = c.encryption(str, key, key2);
+			if(vecResult.empty())
+				throw std::length_error("[Length Error]: Failed to Encrypt File");
+			
+			//write value to file
+			writeValue(argv[4], vecResult);
 
-				if(file.gcount() > 0)
-					str.append(buffer, file.gcount());	//append rest string
-
-				file.close();
-
-				//encrypt logic
-				std::vector<uint64_t> vecResult = c.encryption(str, key, key2);
-				if(vecResult.empty())
-					throw std::length_error("[Length Error]: Failed to Encrypt File");
-				
-				//write to file
-				file.open(argv[4], ios::out | ios::trunc | ios::binary);
-				if(file.fail())
-					throw invalid_argument("[Invalid Input]: Failed to Open File");
-				//write whole vector
-				file.write(reinterpret_cast<const char*>(vecResult.data()), vecResult.size() * sizeof(uint64_t));
-				file.close();
-
-				clog << "[SYSTEM]: SUCCESS TO ENCRYPT FILE" << endl;
-			}
-			else if(argv[2] == "0"sv)
-			{//Decryption
-				//open for Input, for binary, At end
-				file.open(argv[4], ios::in | ios::binary | ios::ate);
-				if(file.fail())
-					throw std::invalid_argument("[Invalid Input]: Failed to Open File");
-				//check file size
-				streamsize size = file.tellg();
-				//return to begin
-				file.seekg(0, ios::beg);
-				//for resize vector
-				size_t count = size / sizeof(uint64_t);
-
-				std::vector<uint64_t> vecInput(count);
-
-				if(count <= 0)
-					throw length_error("[Length Error]: Failed to Read File");
-
-				file.read(reinterpret_cast<char*>(vecInput.data()), size);
-
-				file.close();
-				//decrypt logic
-				str = c.decryption(vecInput, key, key2);
-				
-				file.open(argv[4], ios::out | ios::binary | ios::trunc);
-				if(file.fail())
-					throw invalid_argument("[Invalid Input]: Failed to Open File");
-
-				//write string
-				file.write(str.data(), str.length());
-
-				file.close();
-
-				clog << "[SYSTEM]: SUCCESS TO DECRYPT FILE" << endl;
-			}
-			else	//Invalid Data Type
-				throw std::invalid_argument("[Invalid Input]: -en argument must be 1 or 0");
+			clog << "[SYSTEM]: SUCCESS TO ENCRYPT FILE" << endl;
 		}
-		else	//Invalid parameter input
-			throw invalid_argument("Invalid Input\n[Usage]: ./[file_name] -en [1/0] -f [file_path] -k [key] ([key2])");
+		else if(argv[2] == "0"sv)
+		{//Decryption
+			
+			std::vector<uint64_t> vecInput;
+
+			//read value from file
+			readValue(argv[4], vecInput);
+
+			//decrypt logic
+			str = c.decryption(vecInput, key, key2);
+			if(str.empty())
+				throw std::length_error("[Length Error]: Failed to Decrypt File");
+
+			//write value to file
+			writeValue(argv[4], str);
+
+			clog << "[SYSTEM]: SUCCESS TO DECRYPT FILE" << endl;
+		}
+		else	//Invalid Data Type
+				throw std::invalid_argument("[Invalid Input]: -en argument must be 1 or 0");
 	}catch(const std::exception& e)
 	{
 		cerr << "[Error]: " << e.what() << endl;
@@ -142,4 +95,125 @@ inline bool isStrDigit(const char* msg, int len)
 		if(!isdigit(msg[i]))
 			return false;
 	return true;
+}
+
+inline void chckArgu(int argc, char** argv)
+{
+	//isEncryption: 1, filePath: 1, key: 1
+	if(((argc == 7) || (argc == 8)) && (argv[3] == "-f"sv) && (argv[5] == "-k"sv) && (argv[1] == "-en"sv))
+	{//Standard DES section
+	//foam: ./[file_name] -en [1/0] -f [file_path] -k [key]
+		if(!isValidPath(argv[4]))
+			throw std::invalid_argument("[Invalid Input]: Wrong File Path");
+	}
+	else	//Invalid parameter input
+		throw invalid_argument("Invalid Input\n[Usage]: ./[file_name] -en [1/0] -f [file_path] -k [key] ([key2])");
+}
+
+inline void takeKey(int argc, char** argv, uint64_t& key, uint64_t& key2)
+{
+		//UINT64_MAX = 18,446,744,073,709,551,615 = 0xFFFFFFFFFFFFFFFF
+		try{
+			if(argc == 8)
+				key2 = stoull(argv[7]);
+			else if(argc == 7)
+				key2 = 0;
+
+			key = stoull(argv[6]);
+		}catch(const out_of_range& e)
+		{
+			key = 0;
+			key2 = 0;
+			throw std::overflow_error("[Runtime Error]: Key Value is too Big");
+		}
+		catch(const invalid_argument& e)
+		{
+			key = 0;
+			key2 = 0;
+			throw invalid_argument("[Invalid Input]: Key Value must be ULL Type");
+		}
+}
+
+//ENCRYPTION LOGIC
+inline void readValue(const char* path, std::string& str)
+{
+	//read from file
+	fstream file;
+	char buffer[128];
+
+	//open file for binary -> reduce program running time
+	file.open(path, ios::in | ios::binary);		
+	if(file.fail())
+		throw std::invalid_argument("[Invalid Input]: Failed to Open File");
+	
+	//take file msg
+	while(file.read(buffer, sizeof(buffer)))
+		str.append(buffer, file.gcount());	//take real read count, append that
+	
+	if(file.gcount() > 0)
+		str.append(buffer, file.gcount());	//append rest string
+
+	file.close();
+}
+
+inline void readValue(const char* path, std::vector<uint64_t>& vec)
+{
+	//read from file
+	fstream file;
+
+	//open for Input, for binary, At end
+	file.open(path, ios::in | ios::binary | ios::ate);
+	if(file.fail())
+		throw std::invalid_argument("[Invalid Input]: Failed to Open File");
+
+	//check file size
+	streamsize size = file.tellg();
+	
+	//return to begin
+	file.seekg(0, ios::beg);
+	
+	//for resize vector
+	size_t count = size / sizeof(uint64_t);
+	
+	vec.resize(count);
+
+	if(count <= 0)
+		throw length_error("[Length Error]: Failed to Read File");
+	
+	//take value once to whole vector
+	file.read(reinterpret_cast<char*>(vec.data()), size);
+	
+	file.close();
+}
+
+//ENCRYPTION LOGIC
+inline void writeValue(const char* path, const std::vector<uint64_t> vec)
+{
+	//write to file
+	fstream file;
+	
+	//open file
+	file.open(path, ios::out | ios::trunc | ios::binary);
+	if(file.fail())
+		throw invalid_argument("[Invalid Input]: Failed to Open File");
+	
+	//write whole vector
+	file.write(reinterpret_cast<const char*>(vec.data()), vec.size() * sizeof(uint64_t));
+	file.close();
+}
+
+//DECRYPTION LOGIC
+inline void writeValue(const char* path, const std::string str)
+{
+	//write to file
+	fstream file;
+
+	//open file
+	file.open(path, ios::out | ios::binary | ios::trunc);
+	if(file.fail())
+		throw invalid_argument("[Invalid Input]: Failed to Open File");
+	
+	//write string
+	file.write(str.data(), str.length());
+	file.close();
 }
