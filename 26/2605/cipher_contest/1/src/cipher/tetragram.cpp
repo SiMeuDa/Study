9e6fb9e77fae440a60b2e644207014fcca495648
SiMeuDa <yeskwon05@dgu.ac.kr>
2026-07-17 01:08:31 +0000

    last error

--- 26/2605/cipher_contest/1/src/cipher/tetragram.cpp
+++ 26/2605/cipher_contest/1/src/cipher/tetragram.cpp
@@ -8,7 +8,7 @@
 #include <future>
 #include <cstring>
 #include <string_view>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 #include "cipher.h"
 using namespace std;
@@ -25,20 +25,21 @@ string org_msg(const char*);
 int main(int argc, char* argv[])
 {
 	//klen = key len
-	//div = 
-	//#include <limits>divide number
-	int klen, div = 26;
+	int klen;
+
 	//msg: original message
-	//dir: file directory
-	string msg, dir;
-	vector<double> tetra, result;
+	string msg;
+	
 	//multi threading vector
-	vector<future<vector<double>>> t;
+	vector<future<RESULT>> t;
+	
 	//start, end key point(string)
 	//[Example] thread = 2
 	//sekey[0] == "AAA"
 	//sekey[1] == "NAA"
 	vector<string> sekey;
+
+	//dynamic table
 	unordered_map<string, double> table;
 
 
@@ -50,30 +51,28 @@ int main(int argc, char* argv[])
 	//set sekey size
 	sekey.resize(num_threads + 1);
 	
-	div = div / num_threads;
-	
-	tetra.resize(4, 0.0);
-
 	try{
+		//argument count
 		if(argc != 3)
 			throw invalid_argument("[Usage]: ./[file_name] [comparing msg] [original msg]");
-	
+
+		//setting table
 	 	set_table(argv[1], table);
 		
 		//take key's len
 		take_klen(klen);
 
 		//resize start end point
-		for(int i = 0; i < num_theads; i++)
+		for(int i = 0; i < num_threads; i++)
 		{
 			sekey[i].resize(klen);
 
 			sekey[i] = string(klen, 'A');
 
-			(sekey[i])[0] += (26 / num_theads) * i;
+			(sekey[i])[0] += (26 / num_threads) * i;
 		}
 
-		//set end point "ZZZZZ"
+		//set end point
 		sekey[num_threads] = string(klen, 'Z');
 		
 
@@ -86,84 +85,65 @@ int main(int argc, char* argv[])
     	{	
         	//start lambda threading
 		//msg: original message
-		//tetra: tetragram bias
+		//table: set table on set_table
 		//sekey[i] = start point
 		//sekey[i + 1] = end point
-		//key = using key
-		//dir = directory
 		//klen = key len
-        	t.push_back(async(launch::async, [&msg, tetra, sekey[i], sekey[i + 1], dir, klen]() 
+        	t.push_back(async(launch::async, [&msg, table, sekey[i], sekey[i + 1], klen]() 
 			{
 				//result = saving rseult
 				//chg_key = changing key
 				//gram = tetramgram result
-				string result, chg_key = sekey[i], gram;
+				string chg_key = sekey[i], gram;
+				string_view = sv_msg
+
 				//comp = compare value -> change to max
 				//m = now m value
-				double m = 0, comp = 0;
-				//flags
-				bool isFirst = true;
-				bool isMatch = false;
+				double compare = numeric_limits<double>::min();
+				
 				//saving & return result
 				RESULT res;
-
-				//Simplified Table
-				char table[4][5]= { 
-					"THER",
-					"HERE",
-					"TION",
-					"WITH"
-				};
+				
+				//object for using vigenere
 				cipher c;
+				//string len
 				int len;
 
-				len = msg.length();
-		
-
-				while(true)
+				//start == end -> break
+				while(sekey[i] != sekey[i + 1])
 				{
-					comp = 0;
-					
 					//vigenere == string return
-					result = c.vigenere(msg, chg_key, false);
+					sv_msg = c.vigenere(msg, chg_key, false);
 
-					//divie origin msg to tetra
+					
+					//calculate tetramgram score
+					//high frequency -> big score
+					//low frequency -> small score
 					for(int j = 0; j < len - 3; j++)
 					{
-						isMatch = false;
-						for(int k = 0; k < 4; k++){
-							if(strncmp(result.data() + j, table[k], 4) == 0){
-								isMatch = true;
-								comp += tetra[k];
-								break;
-							}
-						}
-						if(!isMatch)
-							comp += -1;
+						//find value
+						if(table.find(sv_msg.substr(j, 4)) != table.end())
+							score += table[sv_msg.substr(j, 4)];
+						else
+							score += table["!Match"];
+
 					}
-					
-
-					if(isFirst){
-						m = comp;
-						isFirst = false;
-					}
-
-
-					if(comp > m)
+				
+					//find biggest value
+					if(compare < score)
 					{
-						m = comp;
+						//updata compare value
+						compare = score;
 
-						res.score = m;
+						//save return value
+						res.score = score;
 						res.key = chg_key;
 					}
-						
-					clog << "Key: " << chg_key << ", tetra: " << comp << ", m: " << m <<  endl;
-							
 
 					//increase most left key value
 					chg_key[klen - 1]++;
 					
-					//check key
+					//check key valid alphabet value
 					for(int k = klen - 1; k > 0; k--)
 					{		
 						if(chg_key[k] >  'Z')
@@ -177,15 +157,11 @@ int main(int argc, char* argv[])
 
 				return res;
 			}));
-			
-			key[0] += div + 1;
-
-			
 		}
 		
 	bool isFirst = true;
 
-	RESULT final_result = {numeric_limits<double>::min(), "ZZZZZ"};
+	RESULT final_result = {string(klen, 'Z'), numeric_limits<double>::min()};
 
 	    //if it allow to join, join thread
 	    for (auto& it : t)
@@ -200,10 +176,9 @@ int main(int argc, char* argv[])
 		    }
 	
 	    }
-	    
-	    //print result
-	    clog << "[RESULT] Key: " << key << ", Tetragram: " << vecmax[0] << endl;
 
+	    //print result
+	    clog << "[RESULT] Key: " << final_result.key << ", Tetragram: " << final_result.score << endl;
 
 	}catch(const exception& e){
 		cerr << "[Error]: " << e.what() << endl;
@@ -216,12 +191,14 @@ int main(int argc, char* argv[])
 
 void set_table(const char* path, unordered_map<string, double>& table)
 {
+	//file open
 	fstream f;
 	string str;
-	size_t pos = 0;
-	int len = 0, left_point = 0, count = 0;
+	
+	int len = 0, count = 0, min = 0;
 	string left = "";
 	string_view sv_msg;
+
 	f.open(path);
 	if(!f.is_open())
 		throw ios_base::failure("Failed to open File");
@@ -229,38 +206,40 @@ void set_table(const char* path, unordered_map<string, double>& table)
 
 	while(getline(f, str))
 	{
+		//insert left string
 		str.insert(0, left);
 		//erase space
-		for(auto it = str.begin(); it != str.end(); it++)
-			if(*it == ' ')
-				str.erase(it);
+		str.erase(remove(str.begin(), str.end(), ' '), str.end());
 
+		//take length
 		len = str.length();
-		count += len / 4;
-		//if string's length is not 4's multiple
-		if(len % 4)
-		{
-			left_point = len - (len % 4);
-			//save left string
-			for(int i = left_point; i < len; i++)
-				left[i - left_point] = str[i];
-			//erase duplicated string
-			str.erase(left_point);
-			
-			//len = str.length();
-		}
-
+		
+		//copy to string_view
+		//To Prevent additional string copy
 		sv_msg = str;
 
-		for(int i = 0; i < len; i += 4)
-			table[sv_msg.substr(i, 4)] += 1;
+		//take left string
+		left = str.substr(str.length() - 4, 3);
+	
+		//total tetragram count
+		count += len - 3;
+		//table exist, add value
+		for(int i = 0; i < len - 3; i++)
+			table[sv_msg.substr(i, 4).data()] += 1;
 	}
 
 	f.close();
 
+	//set score log
 	for(auto it = table.begin(); it != table.end(); it++)
-		*it->second = log10(*it->second / count);
-
+	{
+		it->second = log10(it->second / count);
+		//find min value
+		if(it->second < min)
+			min = it->second;
+	}
+	
+	table["!Match"] = min - 1.0;
 
 	return;
 }
@@ -268,26 +247,41 @@ void take_klen(int& len)
 {
 	cout << "What is Key's Length: ";
 	int l;
-	cin >> l;
+	cin.exceptions(ios_base::failbit | ios_base::badbit);
+	try{
+		cin >> l;
 	
-	len = l;
+		len = l;
 	
-	if((len >= 1) && (len <= 5))
-		return;
-	else
-		throw range_error("Out of Range");
+		if((len >= 1) && (len <= 5))
+			return;
+		else
+			throw range_error("Out of Range");
+	}
+	catch(const ios_base::failure e){
+		//cin exception
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		throw e.what();
+	}
 }
 
 string org_msg(const char* path)
 {
 	fstream f;
 	f.open(path, ios::in);
+	
 	if(!f.is_open())
 		throw ios_base::failure("Failed to open File");
+
 	string msg;
 	string result;
+	
 	while(getline(f, msg))
 		result.append(msg);
+
+	//erase space
+	result.erase(remove(result.begin(), result.end(), ' '), result.end());
 
 	return result;
 }
